b82567134fa66c94a6a65ced0a993af37d96a2aa
Ian Coelho <ian.coel@hotmail.com>
2022-09-03 11:47:18 -0300

    feat: Add Post entity

---
+++ src/app/database/entity/post.ts
@@ -0,0 +1,46 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  Timestamp,
+  UpdateDateColumn,
+  CreateDateColumn,
+} from 'typeorm'
+
+@Entity()
+class Post extends BaseEntity {
+  @PrimaryGeneratedColumn({
+    type: "integer",
+    comment: "The id of the comment",
+  })
+  post_id: number
+
+  @Column({
+    type: "varchar",
+    comment: "Title of the post",
+  })
+  title: string
+
+  @Column({
+    type: "text",
+    comment: "Description of the post",
+  })
+  description: string
+
+  @CreateDateColumn({
+    type: "timestamp",
+    comment: "Creation data",
+    default: Timestamp,
+  })
+  created_on: Date
+
+  @UpdateDateColumn({
+    type: "timestamp",
+    comment: "Time of Update",
+    default: Timestamp,
+  })
+  updated_on: Date
+}
+
+export default Post
